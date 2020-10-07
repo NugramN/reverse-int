81177817b2dd95df98ed8aa83635aa5a34aec80e
Anastasia Marhun <anastasiamarhun@gmail.com>
2020-10-07 12:38:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    if (n > 0) {
+        result = String(n).split("").reverse().join("");
+    } else {
+        result = String(-1 * n).split("").reverse().join("");
+    }
+    return result;
 }
